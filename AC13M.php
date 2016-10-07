a42a3dc989cf421ce4986685f6e3e1c8afc33dc7
JordanCrenn <crenn.jordan@gmail.com>
2016-10-07 08:43:58 +0200

    AC13 : Fichier Modifier et Ajouter séparés

---
+++ AC13M.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+                      "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr">
+	
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+		<meta http-equiv="content-language" content="fr" />
+		<title>MESGUEN - AC13</title>
+		<link rel="stylesheet" type="text/css" href="default.css" />
+	</head>
+	
+	<body>
+		<form action="AC13traitement.php" method="GET">
+			<label for="Lieu" >Lieu</label>
+			<select size="1" name="lieu" id="lieu">
+				<option value="NULL">Selectionnez un lieu</option>
+				<?php
+					include 'connectAD.php';
+					
+					$TRNNUM = $_GET['tournee'];
+					
+					$sql = "SELECT LIEUID, LIEUNOM
+							FROM lieu";
+					
+					$result = mysql_query($sql);
+					
+					$cpt = mysql_num_rows($result);
+					
+					if ($cpt>0) {	
+						while ($row = mysql_fetch_array($result, MYSQL_BOTH)) {		
+							echo "<option value=$row[0]>$row[1]</option>";
+						}					
+					} else {
+						echo "<select size=\"1\" name=\"numero\" id=\"numero\" disabled=\"disabled\" >";	
+						echo "<option>Aucune information...</option>";
+					}		
+	    		?>   
+	    	</select>
+			
+			<br/>
+			<br/>
+			
+			<label>Rendez-vous entre :</label>
+			<input type="date" name="RDVDebut" id="RDVDebut"/>
+			
+			<label>Et :</label>
+			<input type="date" name="RDVFin" id="RDVFin"/>
+			
+			<br/>
+			<br/>
+			
+			<label>Pris en charge le :</label>
+			<input type="date" value="8/7/15/ 8:15" name="RDVPrise" id="RDVPrise" readonly="readonly"/>
+			
+			<br/>
+			<br/>
+			
+			<label>Commmentaire :</label>
+			<textarea class="commentaire" name="commentaire" id="commentaire" rows="5" cols="15"></textarea>
+			
+			<br/>
+			<br/>
+			
+			
+			
+			<input id="tournee" name="tournee" type="hidden" value="<?php echo "$TRNNUM" ?>" />
+			<input id="valider" name="valider" type="submit" value="Valider"/>
+			<input id="cancel" type="button" name="retour" value="Annuler" onclick="location.href='AC12.php'" />
+		</form>
+	</body>
+
+</html>
---
+++ AC13traitementA.php
@@ -0,0 +1,29 @@
+<?php
+	include 'connectAD.php';
+	
+	$TRNNUM = $_GET['tournee'];
+	$IDLieu = $_GET['lieu'];
+	$Debut = $_GET['RDVDebut'];
+	$Fin = $_GET['RDVFin'];
+	$Commentaire = $_GET['commentaire'];
+	
+	$sql = "SELECT ETPID
+			FROM etape
+			WHERE TRNNUM = '$TRNNUM'";
+	
+	$result = mysql_query($sql);
+	
+	$cpt = compteSQL($sql);
+	
+	$ETPID = $cpt + 1;
+	
+	$sql = "INSERT INTO etape(TRNNUM, ETPID, LIEUID, ETPHREDEBUT, ETPHREFIN, ETPCOMMENTAIRE)
+			VALUES (\"$TRNNUM\", \"$ETPID\",\"$IDLieu\", \"$Debut\", \"$Fin\", \"$Commentaire\");";
+					
+	$result = mysql_query($sql);
+	
+	if ($result)
+		echo "<meta http-equiv='refresh' content='0;url=AC12.php?message=<font color=green> Ajout realisee ! </font> <input id=\"tournee\" name=\"tournee\" type=\"hidden\" value=\"<?php echo \"$TRNNUM\" ?>\" />'>";
+		else
+			echo "<meta http-equiv='refresh' content='0;url=AC12.php?message=<font color=red> Probleme pour ajouter ... </font>'>";
+?>
